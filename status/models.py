002db5ee6bc6a9db97a7c56e7390f9d8ff5219dd
Harry <harry@harrybridge.co.uk>
2021-06-11 13:39:36 +0100

    Add rlm info into DB

--- status/models.py
+++ status/models.py
@@ -64,7 +64,43 @@ class UptimeHistory(models.Model):
         return self.test.name + " - " + self.get_uptime_result_display()
 
 
+class RlmInfo(models.Model):
+    """
+    Contains the results for scraping the RLM server
+    """
+    class Meta:
+        verbose_name = 'RLM info'
+        verbose_name_plural = 'RLM info'
+
+    product = models.CharField(max_length=100)
+    version = models.CharField(max_length=100)
+    count = models.IntegerField()
+    in_use = models.IntegerField()
+    last_updated = models.DateTimeField(default=timezone.now)
+
+    def get_latest_info_dict(self):
+        info = dict()
+        for product in RlmInfo.objects.all().order_by('product'):
+            info[product.product] = dict()
+            prod = info[product.product]
+            prod['product'] = product.product
+            prod['ver'] = product.version
+            prod['count'] = product.count
+            prod['inuse'] = product.in_use
+
+        return info
+
+    def __str__(self):
+        return f"{self.product}, {self.in_use} in use at {self.last_updated}"
+
+
 class ScraperSettings(models.Model):
+    """
+    Holds settings for the web scraper tasks
+    """
+    class Meta:
+        verbose_name_plural = "Scraper settings"
+
     vectorworks_hostname = models.CharField(max_length=100)
     rlm_address = models.CharField(max_length=50)
     rlm_port = models.IntegerField()
