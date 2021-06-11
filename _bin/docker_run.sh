6ae5ff47b84e76e62a4b510acb78396e3146249d
Harry <harry@harrybridge.co.uk>
2021-06-11 11:49:24 +0100

    wait for db management command

--- _bin/docker_run.sh
+++ _bin/docker_run.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
 
-function postgres_ready(){
-python3 << END
-import sys
-import psycopg2
-try:
-    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="$POSTGRES_HOST")
-except psycopg2.OperationalError:
-    sys.exit(-1)
-sys.exit(0)
-END
-}
+# Wait for database to be up
+python manage.py wait_for_db
+sleep 10
 
-until postgres_ready; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "Postgres is up - continuing..."
-
-python3 manage.py migrate                  # Apply database migrations
-
-# start cron
-#echo Starting cron
-#cron && tail -f /var/log/cron.log
-#service cron start
+python manage.py migrate                  # Apply database migrations
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
